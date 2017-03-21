c127bd15d2a5de1c2a2c9def696849c53c3bba7e
KevinTa <loneshark26@yahoo.com>
2017-03-20 20:34:42 -0700

    Step by step

---
+++ lincolnlogsanswer.js
@@ -0,0 +1,15 @@
+var steps=["1. Measure the dimensions of the real Empire State Building.",
+			"2. Measure the parts that protrude from it.",
+			"3. Find out what size the replica is supposed to be. Roughly estimate the height",
+			"4. Then find the ratio of the height of the real building to the replica I'm planning to make.",
+			"5. Use the ratio to find the other dimensions.",
+			"6. Find the measurements of lincoln logs.",
+			"7. Figure out the structure of the replica(mostly hollow, mostly solid, etc.)",
+			"8. Hypothetically sketch out how it would need to be put together in order to create the desired shape and still stay structurally sound.",
+			"9. From that information, calculate how many lincoln logs it would take"
+];
+
+for( var i = 0; i< steps.length;i++)
+{
+	console.log(steps[i]);
+}
